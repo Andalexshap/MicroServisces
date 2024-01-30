ca188814d8b6644fe601675e07f6df677b7fbead
andalexshap <a.shapovalov@infocom-s.ru>
2024-01-30 13:15:45 +0300

    Init

---
+++ MessageApi/Services/MessageService.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WebApiLibrary;
+using WebApiLibrary.Abstraction;
+using WebApiLibrary.DataStore.Entities;
+using WebApiLibrary.DataStore.Models;
+using WebApiLibrary.DataStore.Response;
+
+namespace MessageApi.Services
+{
+    public class MessageService : IMessageService
+    {
+        private readonly Func<AppDbContext> _context;
+        private readonly IMapper _mapper;
+
+        public MessageService(Func<AppDbContext> context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public MessageResponse GetNewMessages(string recipientEmail)
+        {
+            var response = new MessageResponse();
+            using (var context = _context())
+            {
+                var messages = context.Messages
+                    .Include(x => x.Recipient)
+                    .Include(x => x.Sender)
+                    .Where(x => x.Recipient.Email == recipientEmail && !x.IsRead).ToList();
+                
+                foreach (var message in messages)
+                {
+                    message.IsRead = true;
+                }
+
+                context.UpdateRange(messages);
+                context.SaveChanges();
+
+                response.Messages.AddRange(messages.Select(_mapper.Map<MessageModel>));
+                response.IsSuccess = true;
+            }
+            return response;
+        }
+
+        public MessageResponse SendMessage(MessageModel model)
+        {
+            var response = new MessageResponse();
+            using (var context = _context())
+            {
+                var messge = _mapper.Map<MessageEntity>(model);
+                context.Messages.Add(messge);
+                context.SaveChangesAsync();
+
+                response.Messages.Add(model);
+                response.IsSuccess = true;
+            }
+            return response;
+        }
+    }
+}
---
+++ WebApiLibrary/DataStore/Response/MessageResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiLibrary.DataStore.Models;
+
+namespace WebApiLibrary.DataStore.Response
+{
+    public class MessageResponse
+    {
+        public bool IsSuccess { get; set; }
+        public List<MessageModel> Messages { get; set; }
+        public List<ErrorModel> Errors { get; set; }
+    }
+}
